3157129c95b676d0a3b7e1f7faa989a358ea71ea
kleazambau <k.zambau@gmail.com>
2023-12-08 17:56:08 +0300

    Changing client to add file

--- client.py
+++ client.py
@@ -1,4 +1,19 @@
+import requests
+import os
+import socket
+from pathlib import Path
+from decouple import config
+import json
 
+    
+
+#Adding code here
+response = requests.get('https://httpbin.org/ip')
+ip_data = response.json()
+public_ip = ip_data['origin']
+
+BUFFER_SIZE = config("BUFFER_SIZE", cast=int)
+TCP_PORT = config("TCP_PORT", cast=int)
 
 def main():
     print("Welcome to Online Library Catalog Stored in Decentralised Network (delib)")
@@ -14,10 +29,18 @@ def main():
         choice = input("> ").strip()
 
         if choice == "1":
-            pass
+            #Adding code here
+            print("Give file path:")
+            read_file = input()
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect( ("127.0.1.1", TCP_PORT) )
+            s.sendall( json.dumps( {"func": "t_store_file", "filename": read_file} ).encode() )
+            s.close()
         elif choice == "2":
             pass
         elif choice == "3":
+            pass
+        elif choice == "4":
             pass
         else:
             print("Invalid argument provided. Please select one of the options that is offered.")
