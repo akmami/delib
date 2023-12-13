3ce0a17cea2b77ac9399e3c8ac9da0b79bd9b974
yavuz-ozturk <71942103+yavuz-ozturk@users.noreply.github.com>
2023-12-13 04:02:19 +0300

    Update consensus_check.py

--- consensus_check.py
+++ consensus_check.py
@@ -21,7 +21,7 @@ def ask_vote_for_cand_node(cand_node_ip, sender_ip, sender_port, receiver_ip, re
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     s.connect((receiver_ip, sender_port))
 
-    data = {"func": "t_ask_vote_cand_node", "cand_node_ip": cand_node_ip, "receiver_ip": receiver_ip, "receiver_port": receiver_port}
+    data = {"func": "t_ask_vote_cand_node", "cand_node_ip": cand_node_ip, "sender_ip": sender_ip, "sender_port": sender_port, "receiver_ip": receiver_ip, "receiver_port": receiver_port}
     s.sendall( json.dumps(data).encode() )
     
     print("asking vote for candidate node {} - from {}:{}".format(cand_node_ip, receiver_ip, receiver_port) )    
